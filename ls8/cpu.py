b7d69b807ac32e0fe770c70fffefd0d54690a22a
sean-attewell <sean.attewell@gmail.com>
2019-07-22 16:26:31 +0100

    Day 1 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -4,10 +4,33 @@ import sys
 
 class CPU:
     """Main CPU class."""
-
+    
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # r5 = interrupt mask
+        # r6 = interrupt status
+        # r7 = stack pointer
+        self.registers = [0] * 8 # r0 - r7
+        self.running = False
+        self.ram = [0] * 128
+        self.pc = 0
+
+
+    def ram_read(self, MAR):
+        """Read the RAM. MAR = memory address register"""
+        try:
+            return self.ram[MAR]
+        except IndexError:
+            print("index out of range for RAM read")
+
+
+    def ram_write(self, MDR, MAR):
+        """write to the RAM. MDR = Memory Data Register"""
+        try:
+            self.ram[MAR] = MDR
+            print("saved to RAM")
+        except IndexError:
+            print("index out of range for RAM write")
 
     def load(self):
         """Load a program into memory."""
@@ -27,7 +50,7 @@ class CPU:
         ]
 
         for instruction in program:
-            self.ram[address] = instruction
+            self.ram_write(instruction, address)
             address += 1
 
 
@@ -62,4 +85,27 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        self.running = True
+        while self.running:
+            op_code = bin(self.ram_read(self.pc)) # instruction
+
+            if op_code == 0b00000001:# HLT (halt)
+                self.running = False
+                sys.exit(1)
+
+            elif op_code == 0b10000010:  # LDI (load "immediate")
+                data_a = self.ram_read(self.pc + 1)
+                data_b = self.ram_read(self.pc + 2)
+                self.registers[data_a] = data_b
+                self.pc += 3
+                
+            elif op_code == 0b01000111:  # PRN ()
+                data_a = self.ram_read(self.pc + 1)
+                print(self.registers[data_a])
+                self.pc += 1
+                pass
+            else:
+                print(op_code)
+
+
+ 
